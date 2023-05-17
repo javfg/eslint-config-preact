40e98cdbc2e6398e51ed71da9acc0e3bc9b77dc8
Javier Ferrer <javier.f.g@um.es>
2023-05-17 20:52:51 +0200

    Remove babel and jest

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 export default {
-	// TODO: this is really only required for class property initializer methods, which are seeing declining usage.
-	// At some point, we should un-ship the custom parser and let ESLint use esprima.
-	parser: require.resolve('@babel/eslint-parser'),
-
 	// Currently ignored due to the custom parser.
 	parserOptions: {
 		ecmaVersion: 2020,
@@ -22,23 +18,17 @@ export default {
 	},
 
 	// We don't use plugin:react/recommended here to avoid React-specific rules.
-	extends: [
-		'eslint:recommended',
-		'plugin:jest/recommended'
-	],
+	extends: ['eslint:recommended'],
 
-	// TODO: preact-cli ships Jest now, Mocha may be a candidate for removal.
 	plugins: [
 		'compat',
 		'react',
-		'jest',
 		'react-hooks'
 	],
 
 	env: {
 		browser: true,
 		es6: true,
-		'jest/globals': true,
 		node: true
 	},
 
